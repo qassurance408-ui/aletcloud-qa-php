70c7d7c46e6a50287cb7cd9893d0e9229640ab87
qassurance408-ui <qassurance408@gmail.com>
2026-06-26 10:42:51 +0300

    Add PHP extension check to index.php

--- index.php
+++ index.php
@@ -3,3 +3,10 @@ echo "<h1>AletCloud QA - PHP Test</h1>";
 echo "<p>Framework: PHP</p>";
 echo "<p>Server time: " . date('Y-m-d H:i:s') . "</p>";
 ?>
+
+<?php
+$extensions = ['gd', 'intl', 'pdo_mysql', 'pdo_pgsql', 'zip', 'bcmath', 'exif', 'pcntl', 'opcache'];
+foreach ($extensions as $ext) {
+    echo $ext . ': ' . (extension_loaded($ext) ? 'loaded' : 'MISSING') . '<br>';
+}
+?>
